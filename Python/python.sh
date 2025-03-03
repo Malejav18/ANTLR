dcde22e819ba7153fa6524aefcf7fb7c2c72031a
Alevv <134009912+Malejav18@users.noreply.github.com>
2025-03-03 12:27:11 -0500

    Update python.sh

--- Python/python.sh
+++ Python/python.sh
@@ -4,6 +4,10 @@
 ANTLR_JAR="$HOME/Downloads/ANTLR-main/antlr-4.13.1-complete.jar"
 GRAMMAR_FILE="LabeledExpr.g4"
 
+echo "Instalando python con antlr..."
+sudo apt install python3-pip
+sudo pip3 install antlr4-python3-runtime --break-system-packages
+
 # Generar archivos con ANTLR
 echo "Generando archivos con ANTLR..."
 java -jar "$ANTLR_JAR" -Dlanguage=Python3 -visitor "$GRAMMAR_FILE"
